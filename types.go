6ee44d17f5eb3c7304c99de49b78a1cc1dd69efd
Mohamed Yousif <mmbusif@gmail.com>
2020-08-20 21:12:09 +0200

    basic crud in memory

--- types.go
+++ types.go
@@ -1,6 +1,9 @@
 package main
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
 /*
 
@@ -43,46 +46,112 @@ type Storage struct {
 	Due  time.Time // default time.Now() + add(24hrs)
 }
 
-func (s *Storage)NewTask() error{
-	return nil
+func (s *Storage)isValid()bool{
+	if s.Description != "" {
+		return true
+	}
+	return false
 }
-
-func (s *Storage)GetByID(ID int) ( error){
-	return nil
-}
-
-func (s *Storage)UpdateByID(ID int) error{
-	return nil
-}
-
-
-func (s *Storage)Delete(ID int) error{
-return nil
-}
-
 
 
 type User struct {
 	Username string
 	ID int
 	Tasks []Storage
+	Users []User
 }
 
+func (u *User)checkTask(t Storage) (bool){
+	return true
+}
+
+func (u *User)append(t Storage) (bool){
+	if u.checkTask(t){
+		u.Tasks = append(u.Tasks, t)
+		return true
+	}
+	return false
+}
+
+func (u *User)getUser(user string) (*User, error){
+	for _, v := range u.Users{
+		if user == v.Username {
+			return &v, nil
+		}
+	}
+	return &User{}, userNotFound
+}
+
+
 func (u *User)GetUser(username string) (error){
-	return nil
+	res, err := u.getUser(username)
+	u = res
+	return err
 }
 
 func (u *User)GetTasks(username string) ([]Storage, error){
-	return []Storage{}, nil
-}
-func (u *User)GetTaskByID(username string, id int) (Storage, error){
-	return Storage{}, nil
+
+	res, err := u.getUser(username)
+	return res.Tasks, err
+
 }
 
-func (u *User)UpdateTask(username string, id int) error{
-	return nil
+func (u *User)GetTaskByID(username string, id int) (Storage, error){
+	res, err := u.getUser(username)
+	if err != nil {
+		return Storage{}, err
+	}
+
+	for _, v := range res.Tasks {
+		if v.ID == id {
+			return v, nil
+		}
+	}
+	return Storage{}, genericError
 }
+
+
+func (u *User)NewTask(username string, s Storage) error{
+	if s.isValid(){
+		u.Tasks = append(u.Tasks, s)
+		return nil
+	}else{
+		return genericError
+	}
+}
+
+func (u *User)UpdateTask(username string, id int, t Storage) (*Storage, error) {
+	u1, err := u.getUser(username)
+	if err != nil {
+		return nil, err
+	}
+	for _, v := range u1.Tasks {
+		if v.ID == id {
+			v = t
+		}
+	}
+	return nil, userNotFound
+
+}
+
 
 func (u *User)DeleteTask(username string, id int) error{
-	return nil
+	u1, err := u.getUser(username)
+	if err != nil {
+		return err
+	}
+	for k, v := range u1.Tasks {
+		if v.ID == id {
+			u1.Tasks =  append(u1.Tasks[:k], u1.Tasks[k+1:]...)
+			return nil
+		}
+	}
+	return  userNotFound
+
 }
+
+
+var (
+	userNotFound = errors.New("user not found")
+	genericError = errors.New("there is an error")
+)
